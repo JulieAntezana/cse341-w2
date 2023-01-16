605c0067468678831108562c9f87e5515f2d212d
JulieAntezana <jantezana@byui.edu>
2023-01-16 16:03:43 -0600

    fixed errors

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use('/', require('./routes'));
+  .use(require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
+  throw err}
 });
+app.listen(port, () => {
+  console.log(`Connected to DB and listening on ${port}`);
+}
+);
