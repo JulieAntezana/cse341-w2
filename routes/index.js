698ecf21076d1f043d2b51b06c4d05ecda775066
JulieAntezana <jantezana@byui.edu>
2023-02-04 01:49:14 -0600

    update Render connection

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,5 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
-//router.use('/api-doc', require('./api-doc'));
 
 module.exports = router;
--- swagger.js
+++ swagger.js
@@ -18,4 +18,4 @@ const endpointsFiles = ['./routes/index.js'];
 //Run server after it gets generated
 swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
   await import('./server.js');
- });
+});
